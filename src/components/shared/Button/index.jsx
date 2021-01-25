2d05365038fab59b98085d04d35b3461581906ad
Yohanzen Christanto Alexander <yohanzen.alexander@gmail.com>
2021-01-25 23:15:02 +0700

    Add button component

---
+++ src/components/shared/Button/index.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import './Button.scss';
+
+const colors = [
+  'primary', 'light'
+];
+
+const types = [
+  'solid', 'outline'
+];
+
+const sizes = [
+  'small', 'large'
+];
+
+const Button = ({
+  children,
+  type,
+  color,
+  size,
+  disabled = false,
+  ...rest
+}) => {
+
+  type = types.includes(type) ? type : 'solid';
+  color = colors.includes(color) ? color : 'primary';
+  size = sizes.includes(size) ? size : 'small';
+
+  return (
+    <button
+      className={`btn btn-${type}-${color} btn-${size} ${disabled && 'disabled'}`}
+    >
+      {children}
+    </button>
+  )
+}
+
+export default Button;
