4749095f16d1aa8097e75354a98504c9ed74dadc
Yohanzen Christanto Alexander <yohanzen.alexander@gmail.com>
2021-08-15 17:31:03 +0700

    move several assets to cdn

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,10 +1,6 @@
 import sampleUrl from './sample.svg';
-import profileImg from './profile.svg';
 
-export * from './techs';
-export * from './thumbnails';
 export * from './projects';
 export {
   sampleUrl,
-  profileImg,
 };
